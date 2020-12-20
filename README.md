a0a4f6b3455ddafa7a1611a78c1e3aa7097eae15
Alispezzate <maurodarrigo882@gmail.com>
2020-12-20 04:35:20 +0100

    Updates

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GAREN-2.0
  Program used to calculate optimal ultimate damage of Garen from the popular game League Of Legends
 
-The app is pretty straightforward command line app, you just have to insert your ultimate level, the total amount of health and it will display the optimal amount of damage you can make with it. 
+The app is a pretty straightforward command line app, you just have to insert your ultimate level, the total amount of health and it will display the optimal amount of damage you can make with it. 
 
  This is a fan-made app and all rights are reserved to Riot Games based in Santa Monica, California.
--- main.c
+++ main.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    int livello, salutetot;
-    double totale;
-    printf("Qual è il livello della ultimate?\n");
-    scanf("%d", &livello);
-    printf("Qual è la salute totale del tuo avversario?\n");
-    scanf("%d", &salutetot);
+    int Level, TotHealth;
+    int total;
+    printf("What's your ultimate level?\n");
+    scanf("%d", &Level);
+    printf("What's your target total health?\n");
+    scanf("%d", &TotHealth);
 
-    switch (livello)
+    switch (Level)
     {
     case 1:
-        totale=((salutetot + 750) / 6);
-        printf("Il danno ottimale della ultimate %.2f", totale);
+        total=((TotHealth + 750) / 6);
+        printf("The optimal ultimate damage is %d", total);
         break;
     case 2:
-        totale=((salutetot + 1200) / 5);
-        printf("Il danno ottimale della ultimate %.2f", totale);
+        total=((TotHealth + 1200) / 5);
+        printf("The optimal ultimate damage is %d", total);
         break;
     case 3:
-        totale=((salutetot + 1500) * 3 / 13);
-        printf("Il danno ottimale della ultimate %.2f", totale);
+        total=((TotHealth + 1500) * 3 / 13);
+        printf("The optimal ultimate damage is %d", total);
         break;
     default:
-        printf("Livello ultimate non valido");
+        printf("Ultimate level not valid");
         break;
     }
 }
