2638691e9b9c77d5c753fa3a654e0c324c377e84
Alispezzate <maurodarrigo882@gmail.com>
2021-07-10 16:47:31 +0200

    Fixed edge cases

--- main.c
+++ main.c
@@ -17,16 +17,28 @@ int main() {
                     switch (level)
                     {
                     case 1:
+                        if(totHealth<=150){
+                            printf("The optimal ultimate damage is %d\n", totHealth);
+                        }else{
                         total = ((totHealth + 600) / 5);
                         printf("The optimal ultimate damage is %d\n", total);
+                        }
                         break;
                     case 2:
+                        if(totHealth<=300){
+                            printf("The optimal ultimate damage is %d\n", totHealth);
+                        }else{
                         total = ((totHealth + 1000) * 3 / 13);
                         printf("The optimal ultimate damage is %d\n", total);
+                        }
                         break;
                     case 3:
+                        if(totHealth<=450){
+                            printf("The optimal ultimate damage is %d\n", totHealth);
+                        }else{
                         total = ((totHealth + 9000 / 7) * 7 / 27);
                         printf("The optimal ultimate damage is %d\n", total);
+                        }
                         break;
                     default:
                         printf("Ultimate level not valid\n");
